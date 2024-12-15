586a2512da057563dbf1db6ce69e5a1d92526f9d
runyun <runningyun@gmail.com>
2024-12-15 12:54:47 +0800

    Add authentication.

--- routes/base-route.js
+++ routes/base-route.js
@@ -3,21 +3,25 @@ const router = new express.Router();
 const baseController = require('../controllers/base-constoller');
 const validator = require('../utils/validator');
 const validationErrorHandler = require('../utils/validation-error-handler');
+const isAuthenticated = require('../utils/authenticate');
 
 
-router.get('/getUser', baseController.getUser);
+router.get('/getUser',isAuthenticated , baseController.getUser);
 
 router.get('/getUserById',
+    isAuthenticated,
     validator.validateUserId,
     validationErrorHandler,
     baseController.getUserById);
 
 router.get('/getItemsByUserId',
+    isAuthenticated,
     validator.validateUserId,
     validationErrorHandler,
     baseController.getItemsByUserId);
 
 router.get('/getItemByUserIdAndItemName',
+    isAuthenticated,
     validator.validateUserId,
     validator.validateItemName,
     validationErrorHandler,
@@ -26,11 +30,13 @@ router.get('/getItemByUserIdAndItemName',
 
 
 router.post('/createUser',
+    isAuthenticated,
     validator.validateUser,
     validationErrorHandler,
     baseController.addUser);
 
 router.post('/createItem',
+    isAuthenticated,
     validator.validateItem,
     validationErrorHandler,
     baseController.addItem);
@@ -38,12 +44,14 @@ router.post('/createItem',
 
 
 router.put('/updateUser',
+    isAuthenticated,
     validator.validateUserId,
     validator.validateUser,
     validationErrorHandler,
     baseController.updateUser);
 
 router.put('/updateItem',
+    isAuthenticated,
     validator.validateUserId,
     validator.validateItemName,
     validator.validateItem,
@@ -53,14 +61,16 @@ router.put('/updateItem',
 
 
 router.delete('/deleteUser',
+    isAuthenticated,
     validator.validateUserId,
     validationErrorHandler,
     baseController.deleteUser);
 
 router.delete('/deleteItem', 
+    isAuthenticated,
     validator.validateUserId,
     validator.validateItemName,
     validationErrorHandler,
     baseController.deleteItem);
- 
+    
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,58 @@
 const express = require('express')
+const session = require('express-session');
 const app = express()
 const port = 3000
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 require('dotenv').config();
+
+app.use(session(
+    { secret: 'my-secret',
+    resave: false,
+    saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+app.get('/', (req, res) => { 
+  res.send (req.session.user != undefined ? `Logged in as ${req.session.user.username}` : "logged Out")
+});
+
+app.get('/auth/github/callback', passport.authenticate('github', {
+  failureRedirect: '/api-docs', session: false}), 
+  (req, res) =>{
+    req.session.user = req.user;
+    res.redirect('/');
+  }
+);
+
+app.get('/login', passport.authenticate('github', { scope: ['user:email'] }));
+
+app.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
 
 // route
 const baseRoute = require('./routes/base-route');
